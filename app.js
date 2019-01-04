77f9079b10121038b8d813589dbb4ad815d96361
Manjot Singh <manjotsingh16july@gmail.com>
2019-01-04 18:20:23 +0000

    style updated on forms and campground page

--- app.js
+++ app.js
@@ -1,31 +1,46 @@
 var express=require("express");
 var app= express();
+var bodyparser=require("body-parser");
 
+app.use(bodyparser.urlencoded({extended:true}));
 app.set("view engine","ejs");
+
+var campgrounds=[
+            {
+                name:"sharmas leh camp",
+                image:"https://5.imimg.com/data5/GG/GG/GLADMIN-/rajasthan-desert-tour-500x500.jpg"
+            },
+            {
+                name:"Solang campground",
+                image:"http://www.rajasthanvisit.com/Images/CAMPING4.jpg"
+            },
+            {
+                name:"pangong camps",
+                image:"http://www.rajasthanvisit.com/Images/CAMPING4.jpg"
+            }
+        ];
 
 app.get("/",function(req,res){
     res.render("landing");
 });
 
 app.get("/campgrounds",function(req,res){
-    var campgrounds=[
-            {
-                name:"sharmas leh camp",
-                image:"https://cdn.pixabay.com/photo/2016/11/21/15/14/camping-1845906_960_720.jpg"
-            },
-            {
-                name:"Solang campground",
-                image:"https://cdn.pixabay.com/photo/2016/01/19/16/48/teepee-1149402_960_720.jpg"
-            },
-            {
-                name:"pangong camps",
-                image:"https://cdn.pixabay.com/photo/2014/11/27/18/36/tent-548022_960_720.jpg"
-            }
-        ];
+   
     res.render("campgrounds",{campgrounds:campgrounds});
-})
+});
 
+app.post("/campgrounds",function(req,res){
+    var name=req.body.name;
+    var image=req.body.image;
+    var newCampground={name:name,image:image};
+    campgrounds.push(newCampground);
+    res.redirect("/campgrounds");
+    
+});
 
+app.get("/campgrounds/new", function(req, res) {
+    res.render("new");
+});
 
 app.listen(process.env.PORT,process.env.IP,function(){
     console.log("Yelpcamp app started");
