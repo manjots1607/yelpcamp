ae1413d520299c03cd7613424419e7172266ef96
Manjot Singh <manjotsingh16july@gmail.com>
2019-01-17 19:31:33 +0000

    Created models modules and build new comment module

--- app.js
+++ app.js
@@ -1,46 +1,107 @@
-var express=require("express");
-var app= express();
-var bodyparser=require("body-parser");
+var express = require("express"),
+        app = express(),
+ bodyparser = require("body-parser"),
+   mongoose = require("mongoose"),
+  Campground= require("./models/campground"),
+  seedDB    = require("./seed");
+  
+  seedDB();
+   
+   
+ mongoose.connect("mongodb://localhost/yelp_camp",{useNewUrlParser:true});
+
 
 app.use(bodyparser.urlencoded({extended:true}));
 app.set("view engine","ejs");
 
-var campgrounds=[
-            {
-                name:"sharmas leh camp",
-                image:"https://5.imimg.com/data5/GG/GG/GLADMIN-/rajasthan-desert-tour-500x500.jpg"
-            },
-            {
-                name:"Solang campground",
-                image:"http://www.rajasthanvisit.com/Images/CAMPING4.jpg"
-            },
-            {
-                name:"pangong camps",
-                image:"http://www.rajasthanvisit.com/Images/CAMPING4.jpg"
-            }
-        ];
+
+
+// Campground.create(
+//     {
+//                 name:"marwari camps",
+//                 image:"http://www.rajasthanvisit.com/Images/CAMPING4.jpg",
+//                 description:"get the feeling of royal camps alongwith dunes of dessert. We have allbasic facilities available such as water."
+                
+//     },
+//     function(err,newCampground){
+//         if(err){
+//             console.log(err);
+//         }else{
+//             console.log("New campground added: ");
+//             console.log(newCampground);
+//         }
+//     });
+
+// var campgrounds=[
+//             {
+//                 name:"sharmas leh camp",
+//                 image:"https://5.imimg.com/data5/GG/GG/GLADMIN-/rajasthan-desert-tour-500x500.jpg"
+//             },
+//             {
+//                 name:"Solang campground",
+//                 image:"http://www.rajasthanvisit.com/Images/CAMPING4.jpg"
+//             },
+//             {
+//                 name:"pangong camps",
+//                 image:"http://www.rajasthanvisit.com/Images/CAMPING4.jpg"
+//             }
+//         ];
 
 app.get("/",function(req,res){
     res.render("landing");
 });
 
 app.get("/campgrounds",function(req,res){
-   
-    res.render("campgrounds",{campgrounds:campgrounds});
+   Campground.find({},function(err,allCampgrounds){
+       if(err){
+           console.log("Something went wrong");
+           console.log(err);
+       }else{
+           res.render("campgrounds",{campgrounds:allCampgrounds});
+           
+       }
+   })
+    
 });
 
 app.post("/campgrounds",function(req,res){
     var name=req.body.name;
     var image=req.body.image;
-    var newCampground={name:name,image:image};
-    campgrounds.push(newCampground);
-    res.redirect("/campgrounds");
+    var desc=req.body.desc;
+    var newCampground={name:name,image:image,description:desc};
+    Campground.create(newCampground,function(err,newCampground){
+        if(err){
+            console.log("Something went wrong");
+        }else{
+            res.redirect("/campgrounds");
+        }
+    })
+    
     
 });
 
 app.get("/campgrounds/new", function(req, res) {
     res.render("new");
 });
+
+app.get("/campgrounds/:id",function(req,res){
+    var id=req.params.id;
+    Campground.findById(id).populate("comments").exec(function(err,showcamp){
+        if(err){
+            console.log("something went wrong");
+            console.log(err);
+            res.redirect("/campgrounds");
+            
+        }else{
+            console.log(showcamp);
+            
+            res.render("show",{campground:showcamp});
+            
+            
+        }
+    });
+    
+})
 
 app.listen(process.env.PORT,process.env.IP,function(){
     console.log("Yelpcamp app started");
---
+++ seed.js
@@ -0,0 +1,52 @@
+var mongoose = require("mongoose"),
+  Campground = require("./models/campground"),
+  Comment    = require("./models/comment");
+  
+var camps=[
+     {
+        name:"marwari camps",
+        image:"http://www.rajasthanvisit.com/Images/CAMPING4.jpg",
+        description:"get the feeling of royal camps alongwith dunes of dessert. We have allbasic facilities available such as water."
+                
+     },
+     {
+    
+        name:"sharmas leh camp",
+        image:"https://5.imimg.com/data5/GG/GG/GLADMIN-/rajasthan-desert-tour-500x500.jpg",
+        description:"get the feeling of royal camps alongwith dunes of dessert. We have allbasic facilities available such as water."
+            
+     }
+]
+
+function seedDB(){
+    Campground.remove({},function(err){
+        if(err){
+            console.log(err);
+        }else{
+            console.log("Campground deleted");
+            camps.forEach(function(camp){
+               Campground.create(camp,function(err,campdata){
+                   if(err){
+                       console.log(err);
+                   }else{
+                       console.log("added successfully");
+                       Comment.create({
+                           text:"It's the adventure to live in this camp",
+                           author:"david"
+                       },function(err,comment){
+                           if(err){
+                               console.log(err);
+                           }else{
+                               console.log("Added Comment");
+                               campdata.comments.push(comment);
+                               campdata.save();
+                           }
+                       })
+                   }
+               }); 
+            });
+        }
+    });
+    
+}
+module.exports=seedDB;
